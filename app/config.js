b0b81628e020b3ad04b214a707b417a4818b0ddd
Yahor Ptashnik <yahor.ptashnik@LeverX.local>
2026-04-23 10:49:24 +0300

    Small update on base container

---
+++ app/config.js
@@ -0,0 +1,8 @@
+globalThis["sap-ui-config"] ??= {}; // globalThis["sap-ui-config"] could be already available
+globalThis["sap-ui-config"]["theme"] = "sap_horizon";
+globalThis["sap-ui-config"]["bindingsyntax"] = "complex";
+globalThis["sap-ui-config"]["compatversion"] = "edge";
+globalThis["sap-ui-config"]["resourceroots"] = {
+    "planner": "./"
+};
+globalThis["sap-ui-config"]["async"] = "true";
